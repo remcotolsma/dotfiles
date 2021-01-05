6dbc469baf08c347d3410ed26dd1ef8e55f9a621
Remco Tolsma <info@remcotolsma.nl>
2021-01-05 20:43:30 +0100

    Other apps to install.

---
+++ apps.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Remco Tolsma’s apps.
+
+echo 'Remco Tolsma’s applications'
+
+# Install FileZilla
+echo 'Manually install FileZilla'
+
+# Install Local
+echo 'Manually install Local'
